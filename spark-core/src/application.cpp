7256ca96b33b9ee6cc20e92bb1de57c335c07cad
Jérôme Lebel <jerome@fotonauts.com>
2014-05-03 22:49:32 +0200

    using writeAccelFullScaleRange

--- spark-core/src/application.cpp
+++ spark-core/src/application.cpp
@@ -1,11 +1,18 @@
 #include "application.h"
 #include "MPU6050.h"
 #include "Adafruit_NeoPixel.h"
+#include <math.h>
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(16, D2, WS2812B);
+#define PIXEL_COUNT  16
+
+Adafruit_NeoPixel strip1 = Adafruit_NeoPixel(PIXEL_COUNT, D2, WS2812B);
+Adafruit_NeoPixel strip2 = Adafruit_NeoPixel(PIXEL_COUNT, D3, WS2812B);
 MPU6050 mpu = MPU6050(MPU6050::Address0);
 
 uint32_t Wheel(byte WheelPos);
+
+void rainbow(uint8_t modulo, uint8_t wait);
+void ball(void);
 
 void blinkLED(unsigned int count, unsigned int mydelay)
 {
@@ -27,9 +34,9 @@ void setup()
     
     Serial.begin(115200);
     Serial.println("started!");
-    strip.begin();
-    strip.show();
-    blinkLED(16, 250);
+    strip1.begin();
+    strip1.show();
+    blinkLED(4, 250);
     
     Wire.begin();
     Serial.println("prout");
@@ -44,6 +51,8 @@ void setup()
     Serial.print(c,HEX);
     Serial.print(", error = ");
     Serial.println(error,DEC);
+    
+    mpu.writeAccelFullScaleRange(16, NULL);
 
     mpu.writeSleepBit(0, &error);
     Serial.print("PWR_MGMT_1 write : error = ");
@@ -51,60 +60,96 @@ void setup()
 }
 
 void loop() {
-    MPU6050::Values values;
-    
-    mpu.readValues(&values, NULL);
-    Serial.print("accel x,y,z: ");
-    Serial.print(values.xAccel, DEC);
-    Serial.print(", ");
-    Serial.print(values.yAccel, DEC);
-    Serial.print(", ");
-    Serial.print(values.zAccel, DEC);
-    Serial.println("");
-
-
-    // The temperature sensor is -40 to +85 degrees Celsius.
-    // It is a signed integer.
-    // According to the datasheet: 
-    //   340 per degrees Celsius, -512 at 35 degrees.
-    // At 0 degrees: -512 - (340 * 35) = -12412
-
-    Serial.print("temperature: ");
-    double dT = ( (double) values.temperature + 12412.0) / 340.0;
-    Serial.print(dT, 3);
-    Serial.print(" degrees Celsius");
-    Serial.println("");
-
-
-    // Print the raw gyro values.
-
-    Serial.print("gyro x,y,z : ");
-    Serial.print(values.xGyro, DEC);
-    Serial.print(", ");
-    Serial.print(values.yGyro, DEC);
-    Serial.print(", ");
-    Serial.print(values.zGyro, DEC);
-    Serial.print(", ");
-    Serial.println("");
-    
-    Serial.println("ok");
-    delay(1000);
-    //rainbow(20);
+    ball();
+    //rainbow(1, 1);
 }
 
+void ball(void)
+{
+    static double speed = 5;
+    static double friction = 0.01;
+    static double masse = 10;
+    static double position = 0;
+    
+    double pi = 3.14159;
+    MPU6050::Values values;
+    double forceAngle = 00;
+    double forceNorme = 0;
+    uint8_t ii, count, led;
+    Serial.print("position ");
+    Serial.println(position);
+    mpu.readValues(&values, NULL);
+    Serial.print("x accel ");
+    Serial.print(values.xAccel);
+    Serial.print(" y accel ");
+    Serial.println(values.yAccel);
+    values.yAccel = -values.yAccel;
+    if (values.xAccel == 0) {
+        if (values.yAccel < 0) {
+            forceAngle = -90;
+        } else {
+            forceAngle = 90;
+        }
+    } else {
+        forceAngle = atan((double)values.yAccel / (double)values.xAccel) * 180 / pi;
+        if (values.xAccel < 0) {
+            forceAngle -= 180;
+        }
+    }
+    forceNorme = sqrt(values.yAccel * values.yAccel + values.xAccel * values.xAccel);
+    forceNorme = forceNorme / 50000.0;
+    Serial.print("angle ");
+    Serial.print(forceAngle);
+    Serial.print(" diff angle ");
+    Serial.print(position - forceAngle);
+    Serial.print(" force ");
+    Serial.println(sin((position - forceAngle) * pi / 180.0) * forceNorme);
+    speed -= sin((position - forceAngle) * pi / 180.0) * forceNorme;
+    if (speed > 0 && speed > friction) {
+        speed -= friction;
+    } else if (speed < 0 && -speed > friction) {
+        speed += friction;
+    } else {
+        speed = 0;
+    }
+    position += speed;
+    while (position > 360) {
+        position -= 360;
+    }
+    while (position < 0) {
+        position += 360;
+    }
+    count = strip1.numPixels();
+    led = position * count / 360.0;
+    for (ii = 0; ii < count; ii++) {
+        if (led == ii) {
+            strip1.setPixelColor(ii, 0x0f0f0f);
+        } else {
+            strip1.setPixelColor(ii, 0);
+        }
+    }
+    Serial.print("position ");
+    Serial.print(position);
+    Serial.print(" speed ");
+    Serial.print(speed);
+    Serial.print(" led ");
+    Serial.println(led);
+    strip1.show();
+}
 
-void rainbow(uint8_t wait) {
+void rainbow(uint8_t modulo, uint8_t wait)
+{
   uint16_t i, j;
 
   for(j=0; j<256; j++) {
-    for(i=0; i<strip.numPixels(); i++) {
-      if (i % 8 == 0) {
-        strip.setPixelColor(i, Wheel((i+j) & 255));
+    for(i=0; i<strip1.numPixels(); i++) {
+      if (i % modulo == 0) {
+        strip1.setPixelColor(i, Wheel((i+j) & 255));
       } else {
-        strip.setPixelColor(i, 0);
+        strip1.setPixelColor(i, 0);
       }
     }
-    strip.show();
+    strip1.show();
     delay(wait);
   }
 }
@@ -113,12 +158,12 @@ void rainbow(uint8_t wait) {
 // The colours are a transition r - g - b - back to r.
 uint32_t Wheel(byte WheelPos) {
   if(WheelPos < 85) {
-   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+   return strip1.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
   } else if(WheelPos < 170) {
    WheelPos -= 85;
-   return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+   return strip1.Color(255 - WheelPos * 3, 0, WheelPos * 3);
   } else {
    WheelPos -= 170;
-   return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+   return strip1.Color(0, WheelPos * 3, 255 - WheelPos * 3);
   }
 }
